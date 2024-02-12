bdd4d9a3151b558e92787ccf63a105240f314754
Harsh <hs5686584@gmail.com>
2024-02-12 20:46:01 +0530

    js first push

---
+++ Basics/dateTime.js
@@ -0,0 +1,12 @@
+console.log("dates and time concept");
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(typeof myDate);
+// console.log(myDate.toJSON());
+
+let myNewDate = new Date(2023, 0, 31);
+console.log(myNewDate.toDateString());
---
+++ Basics/script.js
@@ -0,0 +1,46 @@
+const nameOfPerson = "harsh";
+let nameOfcity = "noida";
+
+// console.table([nameOfPerson, nameOfcity]);
+
+// types
+
+const personName = "Mr Dead inside"; // string type
+const ageOfPerson = 22; // number type
+const isAdult = false; // boolean type
+const isPowerfull = undefined; // undefined returns undefined type
+const isLoser = null; // null returns an object type
+
+// console.log(typeof personName);
+// console.log(typeof ageOfPerson);
+// console.log(typeof isAdult);
+// console.log(typeof isPowerfull);
+// console.log(typeof isLoser);
+
+// let varOne = "33";
+// let numConvert = Number(varOne);
+// let boolConvert = Boolean(varOne);
+// console.table([varOne, typeof numConvert, numConvert]);
+// console.table([varOne, typeof boolConvert, boolConvert]);
+
+// let varTwo = 1;
+// let varThree = 0;
+// let strConvert = String(varTwo);
+// let booleanConvert = Boolean(varTwo);
+// let booleanConvertTwo = Boolean(varThree);
+
+// console.table([varTwo, typeof strConvert, strConvert]);
+// console.table([varTwo, typeof booleanConvert, booleanConvert]);
+// console.table([varThree, typeof booleanConvertTwo, booleanConvert]);
+
+// let varFour = null;
+// let nullConversion = Boolean(varFour);
+// console.log(varFour, nullConversion);
+
+// let varFour = "hj";
+// let nullConversion = Boolean(varFour);
+// console.log(varFour, nullConversion);
+
+// let varFour = undefined;
+// let nullConversion = Boolean(varFour);
+// console.log(varFour, nullConversion);
