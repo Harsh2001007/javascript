52f7c8379114e8da212bccb7c98aed894385aa97
Harsh <hs5686584@gmail.com>
2024-02-16 00:07:21 +0530

    advance function topics

---
+++ Functions/functionsWithObjArr.js
@@ -0,0 +1,26 @@
+const myObj = {
+  name: "harsh",
+  age: 22,
+  isGamer: true,
+};
+
+function userDetails(passAnyObject) {
+  console.log(
+    `username is -> ${passAnyObject.name}. The age is --> ${passAnyObject.age} and gamer status is ${passAnyObject.isGamer}`
+  );
+}
+
+// userDetails(myObj);
+
+function totalCartPrice(...price1) {
+  console.log(price1);
+}
+
+// totalCartPrice(300, 500, 700, 1000);
+
+const arrToBePassed = [100, 200, 300, 400, 500];
+function getSecondElement(arrName) {
+  console.log(arrName[1]);
+}
+
+getSecondElement(arrToBePassed);
