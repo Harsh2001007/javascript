3323fdf9afa422e42baaa040b910fc16df446614
Harsh <hs5686584@gmail.com>
2024-02-15 23:50:00 +0530

    adding functions notes

---
+++ Functions/functions.js
@@ -0,0 +1,25 @@
+function addNum(num1, num2) {
+  let result = num1 + num2;
+  console.log(result);
+}
+
+// addNum(5, 8);
+// addNum;
+// addNum();
+// addNum(5, "8");
+
+function greeting(name) {
+  return `hello ${name}, welcome to the game`;
+}
+
+greeting("harsh");
+// console.log(greeting("harsh"));
+
+function calling(number) {
+  console.log(`the number is ${number}`);
+  return `i want to call to number ${number}`;
+  console.log(`the number is ${number}`);
+}
+
+calling("8505813979");
+console.log(calling("8700931554"));
