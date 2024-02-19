2317f0265c7b8a7198d7b8dbbf8269a8ccd50794
Harsh <hs5686584@gmail.com>
2024-02-19 23:35:00 +0530

    adding project 1

---
+++ Dom/Project1/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+button.forEach((items) => {
+  items.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "white":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      default:
+        break;
+    }
+  });
+});
